8f37f56a0fea4f31fb966013f787470027323604
Zach Smith <96314181+zsmith357@users.noreply.github.com>
2023-11-11 11:12:15 -0700

    Working without login page

--- dbgazette/miniGazette/urls.py
+++ dbgazette/miniGazette/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from . import views 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('toot/', views.toot, name='toot'),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
 
