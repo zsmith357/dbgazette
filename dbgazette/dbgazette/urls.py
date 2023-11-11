9fdacdca9e279919b98db48a21b4dd710587a0fd
Zach Smith <96314181+zsmith357@users.noreply.github.com>
2023-11-11 11:27:16 -0700

    Login page

--- dbgazette/dbgazette/urls.py
+++ dbgazette/dbgazette/urls.py
@@ -18,20 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-# Import the Django auth views
 from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    # ... other url patterns ...
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    # ... other url patterns ...
-]
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('miniGazette.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 if settings.DEBUG:
--- dbgazette/miniGazette/urls.py
+++ dbgazette/miniGazette/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from . import views 
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', views.home, name='home'),
     path('toot/', views.toot, name='toot'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
